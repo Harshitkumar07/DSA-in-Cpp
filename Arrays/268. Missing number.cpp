e6ced8d65593f1d4ad2b732c0307325b5a922deb
Harshit Kumar <149350677+Harshitkumar07@users.noreply.github.com>
2024-10-28 19:09:34 +0530

    Update 268. Missing number.cpp

--- Arrays/268. Missing number.cpp
+++ Arrays/268. Missing number.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum = 0;
+        int n = nums.size();
+        int ans;
+        for (int i = 0; i < n; i++) {
+            sum = sum + nums[i];
+        }
+        ans = n * (n + 1) / 2 - sum;
+        return ans;
+    }
+};
