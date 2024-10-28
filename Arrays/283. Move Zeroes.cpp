1fae74d10c5168dcba119d0afe88c3b21bc2cc90
Harshit Kumar <149350677+Harshitkumar07@users.noreply.github.com>
2024-10-28 19:10:04 +0530

    Update 283. Move Zeroes.cpp

--- Arrays/283. Move Zeroes.cpp
+++ Arrays/283. Move Zeroes.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int ptr = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                if (i != ptr) {
+                    swap(nums[i], nums[ptr]);
+                }
+                ptr++;
+            }
+        }
+    }
+};
