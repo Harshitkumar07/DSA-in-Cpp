2f13d3c684cc7edffe2d7092fdcac1a3c09e82f7
Harshit Kumar <149350677+Harshitkumar07@users.noreply.github.com>
2024-10-30 16:01:55 +0530

    Update 540. Single element in sorted array.cpp

--- Arrays/540. Single element in sorted array.cpp
+++ Arrays/540. Single element in sorted array.cpp
@@ -6,20 +6,24 @@ public:
         int l = 0, r = n - 1, mid;
         while (l <= r) {
             mid = l + (r - l) / 2;
-            if (mid == 0 || mid == n - 1)
+            if (mid == 0 || mid == n - 1) {
                 break;
-            if (nums[mid] != nums[mid + 1] && nums[mid - 1] != nums[mid])
+            }
+            if (nums[mid] != nums[mid + 1] && nums[mid - 1] != nums[mid]) {
                 break;
+            }        
             if (mid % 2 == 0) {
-                if (nums[mid + 1] == nums[mid])
+                if (nums[mid + 1] == nums[mid]) {
                     l = mid + 1;
-                else
+                } else {
                     r = mid - 1;
+                }
             } else {
-                if (nums[mid - 1] == nums[mid])
+                if (nums[mid - 1] == nums[mid]) {
                     l = mid + 1;
-                else
+                } else {
                     r = mid - 1;
+                }
             }
         }
         return nums[mid];
