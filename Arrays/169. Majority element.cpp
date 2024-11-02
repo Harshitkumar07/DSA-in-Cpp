e4ae174ef6361c35b90f4a1fb47b471a59ffc499
Harshit Kumar <149350677+Harshitkumar07@users.noreply.github.com>
2024-11-02 12:10:32 +0530

    Update 169. Majority element.cpp

--- Arrays/169. Majority element.cpp
+++ Arrays/169. Majority element.cpp
@@ -1,3 +1,18 @@
+Leetcode Problem 169:-
+
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+Example 1:
+Input: nums = [3,2,3]
+Output: 3
+    
+Example 2:
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+    
+Code:-
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
@@ -17,3 +32,11 @@ public:
         return ans;
     }
 };
+
+Time Complexity:
+The time complexity is O(n) because we iterate through the nums array exactly once. Each iteration performs constant-time operations (updating freq and possibly ans), 
+resulting in a linear pass over n elements, where n is the size of nums.
+
+Space Complexity:
+The space complexity is O(1) since we use only a fixed amount of extra space for variables (freq and ans). 
+No additional data structures are created that depend on the input size, so the space usage remains constant.
