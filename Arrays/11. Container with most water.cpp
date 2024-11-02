344fb473c57403505d2a0257671e3afa8e97e6e8
Harshit Kumar <149350677+Harshitkumar07@users.noreply.github.com>
2024-11-02 12:14:07 +0530

    Update 11. Container with most water.cpp

--- Arrays/11. Container with most water.cpp
+++ Arrays/11. Container with most water.cpp
@@ -1,18 +1,19 @@
-Leetcode Problem 11:-
+**Leetcode Problem 11:-**
+
 You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
 Find two lines that together with the x-axis form a container, such that the container contains the most water.
 Return the maximum amount of water a container can store.
 
-Example 1:
+**Example 1:**
 Input: height = [1,8,6,2,5,4,8,3,7]
 Output: 49
 Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
 
-Example 2:
+**Example 2:**
 Input: height = [1,1]
 Output: 1
 
-Code:-
+**Code:-**
     
 class Solution {
 public:
@@ -33,10 +34,10 @@ public:
     }
 };
 
-Time Complexity:
+**Time Complexity:**
 The time complexity is O(n) because we only traverse the array once. Each iteration either increments lp or decrements rp, meaning we only perform n - 1 comparisons and moves,
 where n is the number of elements in height.
 
-Space Complexity:
+**Space Complexity:**
 The space complexity is O(1) since we use only a fixed amount of extra space for pointers (lp, rp) and variables (maxWater, water, ht, currWater).
 No additional data structures are needed.
