f8685715e27a3f557ac0da22aa3007746afd89db
Harshit Kumar <149350677+Harshitkumar07@users.noreply.github.com>
2024-12-09 06:55:05 +0530

    Update and rename 4Sum to 4Sum.cpp

--- Arrays/Leetcode Solutions/4Sum.cpp
+++ Arrays/Leetcode Solutions/4Sum.cpp
@@ -1,6 +1,18 @@
 ** Leetcode Problem 18. :- **
 
+Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+0 <= a, b, c, d < n
+a, b, c, and d are distinct.
+nums[a] + nums[b] + nums[c] + nums[d] == target
+You may return the answer in any order.
 
+Example 1:
+Input: nums = [1,0,-1,0,-2,2], target = 0
+Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+
+Example 2:
+Input: nums = [2,2,2,2,2], target = 8
+Output: [[2,2,2,2]]
 
 ** Code :- **
 
@@ -38,3 +50,21 @@ public:
     }
 };
 
+** Time Complexity :- **
+
+Outer Loops (i and j):
+The first loop runs O(n) times for i, and the second loop runs O(n) times for j in the worst case.
+Combined, this results in O(n2) iterations.
+Two-Pointer Search (p and q):
+For each (i,j) pair, the two-pointer technique processes O(n) elements in total.
+
+Overall Time Complexity: O(n3)
+Where n is the size of the input array.
+
+** Space Complexity :- **
+
+Sorting: Sorting the array requires O(logn) space due to the sorting algorithm's stack space (if quicksort is used).
+Auxiliary Space:
+The algorithm uses O(1) additional space for variables and O(k) space for the result, where k is the number of quadruplets.
+
+Overall Space Complexity: O(k+logn)
